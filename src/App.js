eb3973fbfdd6d6cc011888de26b45a17d34a0855
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-01-29 04:18:45 -0600

    MVP MET Because: --{Followed All Instructions in One Run} --{Stopped At Stretch Since Everything was Going Smooth} --{Looking at Stretch in the AM}
    This commit:
    ~~Commits my MVP Working Correctly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-
+//ContextAPI
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -11,31 +13,30 @@ function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
+	const addItem = () => {
+		 setCart(products);
 	};
 
 	return (
+		<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value= {cart}>
 		<div className="App">
-			<Navigation cart={cart} />
+			<Navigation/>
 
 			{/* Routes */}
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				component={Products}
 			/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+					<Route
+						path="/cart"
+						component={ShoppingCart}
 			/>
-		</div>
+				</div>
+			</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
